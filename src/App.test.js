f2b4028b3a63402581a2aa8d32be31c5258a4789
vibhurai <kaustubhrai2000@gmail.com>
2020-12-10 20:38:52 +0530

    lews testing done

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Mainpage from './MainPage'
-import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
-import { useStoreActions } from 'easy-peasy';
-import useLoader from './hooks/useLoader';
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-//   const div = document.createElement("div")
-//   ReactDOM.render(<App></App>, div)
-  expect(true).toBe(true);
-  // const div = document.createElement("div")
-  // ReactDOM.render(<Mainpage></Mainpage>, div)
-// const tree = renderer.create(<Mainpage></Mainpage>).toJSON();
-// expect(tree).toMatchSnapshot();
+
+test('checks the working of the website', () => {
+    const div = document.createElement("div")
+    const tree = renderer.create(<App/>).toJSON();
+    expect(tree).toMatchSnapshot();
 });
