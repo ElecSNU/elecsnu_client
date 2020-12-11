ce7e350ab62fb830091a706125b2a90d25320dfe
vibhurai <kaustubhrai2000@gmail.com>
2020-12-11 14:42:02 +0530

    Homepage alighment left

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,26 +3,47 @@ import React from 'react';
 import './HomeArt.css';
 
 import homeArt from '../../assets/images/home-art.svg';
+import sc1 from '../../assets/images/scroll_1.png';
+import sc2 from '../../assets/images/scroll_2.png';
+// import gfont from 'https://fonts.googleapis.com/css?family=Montserrat' 
 
 const HomePage = () => {
+   
     return (
-        <div
-            id='home'
-            style={{
-                width: '100%',
-                height: '100%',
-            }}
-        >
-            <div className='voting-banner'>
-                <h1>Let's make voting fun!</h1>
-                <button className='submit-button'>
-                    Vote Now!
-                </button>
+        <>
+            <div
+                id='home'
+                style={{
+                    width: '100%',
+                    height: '100vh',
+                    backgroundColor: '#004aad'
+                }}
+            >
+                <div className='voting-banner' >
+                        <h1  style={{
+                        // fontFamily: "Montserrat"
+                    }}>LET'S MAKE VOTING FUN!</h1>
+                        <br/>
+                        <h4 >
+                            A step towards secure and intuitive digital voting 
+                        </h4>
+
+                </div>
+                <div className='home-art'>
+                    <img src={homeArt} alt='home-art' />
+                </div>
             </div>
-            <div className='home-art'>
-                <img src={homeArt} alt='home-art' />
+            {/* <div className='sc1'> */}
+                <h1>All about online voting systems</h1>     
+            {/* </div>s */}
+            <div className='sc1'>
+                <img src={sc1} alt='sc1' />    
             </div>
-        </div>
+            <div className='sc1'>
+                <img src={sc2} alt='sc1' />    
+            </div>
+            
+        </>
     );
 };
 
